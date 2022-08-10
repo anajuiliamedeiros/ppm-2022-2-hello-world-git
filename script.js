cd7fd282623427125a251563e302957dbf1b1fa5
Ana Julia Medeiros <anajuliammarcelino@gmail.com>
2022-08-09 21:01:18 -0300

    Finalizada js helloWorld

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 document.querySelector('#cliqueAqui').onclick = () => {
-    const nome =document.querySelector('#campoNome').value;
+    const nome = document.querySelector('#campoNome').value;
+    if(nome){
+        alert(`Hello Wolrd, ${nome}!`);
+    }
     console.log(nome);
 }
