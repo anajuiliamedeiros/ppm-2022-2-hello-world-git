a8f76437de71cc2bb8616b79d2d7989bea5009b2
Ana Julia Medeiros <anajuliammarcelino@gmail.com>
2022-08-09 21:17:42 -0300

    Criando limpar

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.querySelector('#cliqueAqui').onclick = () => {
     const nome = document.querySelector('#campoNome').value;
     if(nome){
         alert(`Hello Wolrd, ${nome}!`);
-    }
-    console.log(nome);
-}
+    };
+    
+};
+
+document.querySelector('#limpar').onclick = () => {
+    document.querySelector('#campoNome').value = '';  
+};
